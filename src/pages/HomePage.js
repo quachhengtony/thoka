f50615ff4f49a623c947aed0dce6198a605c4c7d
TonyInfinity <quachhengtony@gmail.com>
2021-02-23 21:26:33 +0700

    Changed primary color.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -8,7 +8,7 @@ function HomePage() {
   return (
     <div className="homepage">
       <div className="content">
-        <div className="container-xl">
+        {/* <div className="container-xl">
           <div className="page-header">
             <div className="row align-items-center">
               <h3>Phase 1: Thoka | Collaboration solution & Team social channel</h3>
@@ -18,51 +18,14 @@ function HomePage() {
               </h5>
               <h5>- Mạng truyền thông nội bộ cho teams. Facebook page cho toàn startup (Home) và facebook groups riêng cho từng teams (Teams). Kênh tin tức, thông báo... (Announcements)</h5>
               <h5>(PR tập trung vào Startups)</h5>
-              {/* <div
-                className="alert alert-success alert-dismissible"
-                role="alert"
-              >
-                <h3 className="mb-1">Thoka.io Team</h3>
-                <p>
-                  This is a work in progress pre-alpha. Our website is currently
-                  under construction. Sign up and we'll notify you when we
-                  launch.
-                </p>
-                <div className="btn-list">
-                  <button
-                    onClick={() => history.push("/signup")}
-                    className="btn btn-success"
-                  >
-                    Okay
-                  </button>
-                </div>
-              </div> */}
             </div>
           </div>
-          {/* <div className="homepage__hero">
-            <div className="alert alert-success alert-dismissible" role="alert">
-              <h3 className="mb-1">Thoka.io Team</h3>
-              <p>
-                This is a work in progress pre-alpha. Our website is currently
-                under construction. Sign up and we'll notify you when we launch.
-                We wish you a happy Valentine's Day!
-              </p>
-              <div className="btn-list">
-                <button
-                  onClick={() => history.push("/signup")}
-                  className="btn btn-success"
-                >
-                  Okay
-                </button>
-              </div>
-            </div>
-          </div> */}
-        </div>
+        </div> */}
         <div className="container-xl">
           <div className="homepage__footer">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
               <path
-                fill="#206BC4"
+                fill="#0061FE"
                 fill-opacity="1"
                 d="M0,96L48,85.3C96,75,192,53,288,85.3C384,117,480,203,576,224C672,245,768,203,864,160C960,117,1056,75,1152,48C1248,21,1344,11,1392,5.3L1440,0L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
               ></path>
